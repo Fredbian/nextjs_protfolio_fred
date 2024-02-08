073b4d0ef51ee8d47417e01cdd6cdad1d623bb73
Tao Bian <biantao0519@hotmail.com>
2024-02-09 09:51:31 +1100

    deploy app

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -5,11 +5,11 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
-  
+
   const { email, subject, message } = await req.json();
 
   console.log(email, subject, message);
-  
+
   try {
     const data = await resend.emails.send({
       from: fromEmail,
@@ -29,3 +29,26 @@ export async function POST(req, res) {
     return NextResponse.json({ error });
   }
 }
+
+
+// TEST ONLY
+// export async function GET() {
+//   const resend = new Resend(process.env.RESEND_API_KEY)
+    
+//   try {
+//     const {data, error} = await resend.emails.send({
+//       from: 'biantao0519@gmail.com',
+//       to: 'biantao0519@gmail.com',
+//       subject: 'Hello from Next.js',
+//       html: '<h1>Hello from Next.js</h1>'
+//     })
+
+//     if (error) {
+//       return Response.json({error})
+//     }
+
+//     return NextResponse.json({data})
+//   } catch (error) {
+//     return NextResponse.json({ error })
+//   }
+// }
