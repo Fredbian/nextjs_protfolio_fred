2f166ea255e987cd638956c21d76798ba11d6863
Tao Bian <biantao0519@hotmail.com>
2024-02-08 15:24:15 +1100

    Added 'AchievementsSection and EmailSection', change some styles

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -5,6 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
+  
   const { email, subject, message } = await req.json();
 
   console.log(email, subject, message);
--- components/HeroSection/index.jsx
+++ components/HeroSection/index.jsx
@@ -6,11 +6,11 @@ import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
   return (
-    <section>
+    <section className='lg:py-16'>
       <div className="grid grid-cols-1 sm:grid-cols-12">
-        <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className="text-white text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-[#FFC300]">
+        <div className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
+          <h1 className="text-white text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-600 to-secondary">
               Hello, I'm {''}
             </span>
             <br />
@@ -36,17 +36,17 @@ const HeroSection = () => {
             blanditiis dolorum dolores error.
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 hover:bg-gradient-to-tl bg-gradient-to-br from-blue-600 via-[#959556] to-[#FFC300] hover:bg-slate-200 text-white">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 hover:bg-gradient-to-tl bg-gradient-to-br from-primary-600 via-secondary to-Tertiary hover:bg-slate-200 text-white">
               Hire Me
             </button>
-            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white mt-3 bg-gradient-to-br from-blue-600 via-[#959556] to-[#FFC300]">
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white mt-3 bg-gradient-to-br from-primary-600 via-secondary to-Tertiary">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2 ">
                 Download CV
               </span>
             </button>
           </div>
         </div>
-        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+        <div className="col-span-4 place-self-center mt-4 lg:mt-0">
           <Image
             src="/images/hero-image.png"
             alt="hero image"
