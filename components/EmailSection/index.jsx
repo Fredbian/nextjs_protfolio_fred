ad5ddf95d19a388b155a50e4d8e95a288a1d35c6
Tao Bian <biantao0519@hotmail.com>
2024-02-09 17:16:33 +1100

    Working on 'EmailSection'

--- components/EmailSection/index.jsx
+++ components/EmailSection/index.jsx
@@ -1,77 +1,84 @@
-"use client";
-import React, { useState } from "react";
-import GithubIcon from "../../public/github-icon.svg";
-import LinkedinIcon from "../../public/linkedin-icon.svg";
-import Link from "next/link";
-import Image from "next/image";
+'use client';
+import React, { useState, useRef } from 'react';
+import GithubIcon from '../../public/github-icon.svg';
+import LinkedinIcon from '../../public/linkedin-icon.svg';
+import Link from 'next/link';
+import Image from 'next/image';
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const emailServiceId = process.env.EMAIL_SERVICES_ID
+const emailTemplateId = process.env.EMAIL_TEMPLATE_ID
+const emailPublicKey = process.env.EMAIL_PUBLIC_KEY
+
 
 const EmailSection = () => {
   const [emailSubmitted, setEmailSubmitted] = useState(false);
 
+  const form = useRef();
+
+  const successMsg = () => {
+    toast.success('Message Sent!');
+  };
+
+  const unSuccessMsg = (msg) => {
+    toast.error(msg);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const data = {
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
-
-    // Form the request for sending data to the server.
-    const options = {
-      // The method is POST because we are sending data.
-      method: "POST",
-      // Tell the server we're sending JSON.
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // Body of the request is the JSON data we created above.
-      body: JSONdata,
-    };
-
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
-
-    if (response.status === 200) {
-      console.log("Message sent.");
-      setEmailSubmitted(true);
-    }
-
+    emailjs
+      .sendForm(
+        emailServiceId,
+        emailTemplateId,
+        form.current,
+        {
+          publicKey: emailPublicKey,
+        }
+      )
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          successMsg();
+          e.target.reset();
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          unSuccessMsg(error.text);
+        }
+      );
   };
 
   return (
-    <section
-      id="contact"
-      className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
-    >
-      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-600 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
-      <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">
-          Let&apos;s Connect
-        </h5>
-        <p className="text-[#ADB7BE] mb-4 max-w-md">
-          {" "}
-          I am currently looking for new opportunities, my inbox is always
-          open. Whether you have a question or just want to say hi, I will
-          try my best to get back to you!
-        </p>
-        <div className="socials flex flex-row gap-2">
-          <Link href="https://github.com/Fredbian">
-            <Image src={GithubIcon} alt="Github Icon" />
-          </Link>
-          <Link href="https://www.linkedin.com/in/tao-bian-9aa137239/">
-            <Image src={LinkedinIcon} alt="Linkedin Icon" />
-          </Link>
-        </div>
-      </div>
-      <div>
-        {emailSubmitted ? (
-          <p className="text-green-500 text-sm mt-2">
-            Email sent successfully!
+    <>
+      <ToastContainer position='top-right' />
+      <section
+        id="contact"
+        className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
+      >
+        <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-600 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
+        <div className="z-10">
+          <h5 className="text-xl font-bold text-white my-2">
+            Let&apos;s Connect
+          </h5>
+          <p className="text-[#ADB7BE] mb-4 max-w-md">
+            {' '}
+            I am currently looking for new opportunities, my inbox is always
+            open. Whether you have a question or just want to say hi, I will try
+            my best to get back to you!
           </p>
-        ) : (
-          <form className="flex flex-col" onSubmit={handleSubmit}>
+          <div className="socials flex flex-row gap-2">
+            <Link href="https://github.com/Fredbian">
+              <Image src={GithubIcon} alt="Github Icon" />
+            </Link>
+            <Link href="https://www.linkedin.com/in/tao-bian-9aa137239/">
+              <Image src={LinkedinIcon} alt="Linkedin Icon" />
+            </Link>
+          </div>
+        </div>
+        <div>
+          <form ref={form} className="flex flex-col" onSubmit={handleSubmit}>
             <div className="mb-6">
               <label
                 htmlFor="email"
@@ -80,9 +87,9 @@ const EmailSection = () => {
                 Your email
               </label>
               <input
-                name="email"
+                name="user_email"
                 type="email"
-                id="email"
+                id="user_email"
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                 placeholder="jacob@google.com"
@@ -93,15 +100,15 @@ const EmailSection = () => {
                 htmlFor="subject"
                 className="text-white block text-sm mb-2 font-medium"
               >
-                Subject
+                Your Name
               </label>
               <input
-                name="subject"
+                name="user_name"
                 type="text"
-                id="subject"
+                id="user_name"
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="Just saying hi"
+                placeholder="Jacob"
               />
             </div>
             <div className="mb-6">
@@ -125,9 +132,9 @@ const EmailSection = () => {
               Send Message
             </button>
           </form>
-        )}
-      </div>
-    </section>
+        </div>
+      </section>
+    </>
   );
 };
 
